8dbc8b1bbb1b01e702be37599ca9326c53092c18
kristinabobako <kristinabobako@mail.ru>
2024-01-17 14:50:01 +0300

    Генератор предсказаний

--- script.js
+++ script.js
@@ -13,3 +13,56 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const forecastBtn = document.querySelector(".forecast-btn"); 
+const currentForecastPrediction = document.querySelector("h1");
+const currentForecastPercend = document.querySelector("p");
+
+const template = document.querySelector("#forecast-item");
+const myPredictionContainer = document.querySelector(".forecasts");
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function insertMyPredictions(predictionText, predictionPercentText) {
+    const myPredictions = template.content.cloneNode(true);
+
+    myPredictions.querySelector("h3").textContent = predictionText;
+    myPredictions.querySelector("p").textContent = predictionPercentText;
+
+    return myPredictions;
+};
+
+forecastBtn.addEventListener('click', function() {
+
+    const predictionRandom = getRandom(1, 6);
+    let predictionText = "";
+
+    if (predictionRandom == 1) {
+        predictionText = "Удача придет откуда не ждешь!";
+    } else if (predictionRandom == 2) {
+        predictionText = "Яркое приключение уже поджидает тебя!";
+    } else if (predictionRandom == 3) {
+        predictionText = "Жизнь пополнится еще одной радостью!";
+    } else if (predictionRandom == 4) {
+        predictionText = "Получишь хорошую новость!";
+    } else if (predictionRandom == 5) {
+        predictionText = "Тебя ожидают приятные хлопоты!";
+    } else {
+        predictionText = "Кто-то поделиться с тобой позитивом!";
+    };
+
+    currentForecastPrediction.textContent = predictionText; //
+
+    let predictionPercent = getRandom(1, 101);
+
+    let predictionPercentText = `Вероятность исполнения предсказания ${predictionPercent}%.`;
+
+    currentForecastPercend.textContent = predictionPercentText; //
+
+    forecastBtn.addEventListener('click', function() {
+        const oldPrediction = insertMyPredictions(predictionText, predictionPercentText);
+        myPredictionContainer.prepend(oldPrediction);
+    });
+});
